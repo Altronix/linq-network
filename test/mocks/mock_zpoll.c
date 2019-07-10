bda599551065c62be079e8d97d727fd959d2f5ce
thomas <thomas.chiantia@gmail.com>
2019-07-10 08:34:59 -0400

    added wrap poll

---
+++ test/mocks/mock_zpoll.c
@@ -0,0 +1,23 @@
+#include "mock_zpoll.h"
+#include "zmq.h"
+
+static bool incoming = false;
+
+void
+czmq_spy_poll_push_incoming(bool i)
+{
+    incoming = i;
+}
+
+int
+__wrap_zmq_poll(zmq_pollitem_t* items_, int nitems_, long timeout_)
+{
+    ((void)nitems_);
+    ((void)timeout_);
+    if (incoming) {
+        items_->revents |= ZMQ_POLLIN;
+    } else {
+        items_->revents &= (~(ZMQ_POLLIN));
+    }
+    return 0;
+}
---
+++ test/mocks/mock_zpoll.h
@@ -0,0 +1,17 @@
+#ifndef MOCK_ZPOLL_H_
+#define MOCK_ZPOLL_H_
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C"
+#endif
+
+    void
+    czmq_spy_poll_push_incoming(bool i);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
