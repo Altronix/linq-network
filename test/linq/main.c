25ae50df219189763fd49b47615e93beec339ed2
thomas <thomas.chiantia@gmail.com>
2019-07-10 13:08:46 -0400

    heartbeat test

--- test/linq/main.c
+++ test/linq/main.c
@@ -8,9 +8,9 @@
 #include <setjmp.h>
 
 static e_linq_error expect_error = e_linq_ok;
-static char* empty = "";
-static char* expect_what = "";
-static char* expect_serial = "";
+static const char* empty = "";
+static const char* expect_what = "";
+static const char* expect_serial = "";
 
 static void
 test_reset()
@@ -73,6 +73,27 @@ test_linq_receive_heartbeat_ok(void** context_p)
     ((void)context_p);
 }
 
+static void
+test_linq_receive_heartbeat_error_short(void** context_p)
+{
+    ((void)context_p);
+    bool pass = false;
+    linq* l = linq_create(&callbacks, (void*)&pass);
+    zmsg_t* m = helpers_create_message_mem(
+        4, "router", 6, "\x0", 1, "\x0", 1, "product", 7);
+
+    expect_error = e_linq_protocol;
+    czmq_spy_mesg_push_incoming(&m);
+    czmq_spy_poll_push_incoming(true);
+
+    linq_poll(l);
+
+    assert_true(pass);
+
+    linq_destroy(&l);
+    test_reset();
+}
+
 int
 main(int argc, char* argv[])
 {
@@ -82,6 +103,7 @@ main(int argc, char* argv[])
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_linq_create),
         cmocka_unit_test(test_linq_receive_heartbeat_ok),
+        cmocka_unit_test(test_linq_receive_heartbeat_error_short),
         cmocka_unit_test(test_linq_receive_protocol_error_short)
     };
 
