0c66625fe33d6922dacb2e50fa68f2b0607559ff
thomas <thomas.chiantia@gmail.com>
2019-07-12 12:16:57 -0400

    move dirs

--- cmake/test-fixture.cmake
+++ cmake/test-fixture.cmake
@@ -9,19 +9,6 @@ add_library(http-parser
 	${http_parser_SOURCE_DIR}/http_parser.c 
 	${http_parser_SOURCE_DIR}/http_parser.h)
 target_include_directories(http-parser PUBLIC ${http_parser_SOURCE_DIR})
-# ExternalProject_Add(http_parser_PROJECT
-# 	SOURCE_DIR ${http_parser_SOURCE_DIR}
-# 	INSTALL_DIR ${deps_INSTALL_DIR}
-# 	CONFIGURE_COMMAND ""
-# 	BUILD_COMMAND make package PREFIX=${deps_INSTALL_DIR}
-# 	INSTALL_COMMAND make install)
-# ExternalProject_Get_Property(http_parser_PROJECT install_dir)
-# set(http_parser_LIBRARY ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}http-parser${CMAKE_STATIC_LIBRARY_SUFFIX})
-# set(http_parser_INCLUDE_DIR ${install_dir}/include)
-# add_library(http_parser STATIC IMPORTED)
-# add_dependencies(http_parser http_parser_PROJECT)
-# set_property(TARGET http_parser PROPERTY IMPORTED_LOCATION ${http_parser_LIBRARY})
-# set_property(TARGET http_parser PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${http_parser_INCLUDE_DIR})
 
 ### Build Wolfssl ###
 ExternalProject_Add(wolfssl_PROJECT
@@ -49,4 +36,46 @@ add_dependencies(wolfssl wolfssl_PROJECT)
 set_property(TARGET wolfssl PROPERTY IMPORTED_LOCATION ${wolfssl_LIBRARY})
 set_property(TARGET wolfssl PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${wolfssl_INCLUDE_DIR})
 
-### TODO ### build linq-common
+### build linq-common ###
+ExternalProject_Add(linq_common_PROJECT
+	SOURCE_DIR ${linq_common_SOURCE_DIR}
+	PREFIX ${deps_INSTALL_DIR})
+ExternalProject_Get_Property(linq_common_PROJECT install_dir)
+
+ExternalProject_Add(atx-project
+	SOURCE_DIR ${linq_common_SOURCE_DIR}
+	UPDATE_COMMAND ""
+	LIST_SEPARATOR |
+	CMAKE_ARGS 
+		-DCMAKE_INSTALL_PREFIX=${deps_INSTALL_DIR} 
+		-DZMQ_BUILD_TESTS:BOOL=OFF 
+		-DBUILD_TESTS:BOOL=OFF 
+		-DBUILD_STATIC:BOOL=ON
+		-DBUILD_WOLFSSL:BOOL=OFF
+		-DBUILD_HTTP_PARSER:BOOL=OFF
+		-DCMAKE_BUILD_TYPE=Debug
+	)
+
+add_library(atx STATIC IMPORTED)
+add_library(osal STATIC IMPORTED)
+add_library(crypt STATIC IMPORTED)
+add_library(item STATIC IMPORTED)
+add_library(uzmtp STATIC IMPORTED)
+
+set_property(TARGET atx PROPERTY IMPORTED_LOCATION ${deps_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}atx${CMAKE_STATIC_LIBRARY_SUFFIX})
+set_property(TARGET osal PROPERTY IMPORTED_LOCATION ${deps_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}osal${CMAKE_STATIC_LIBRARY_SUFFIX})
+set_property(TARGET crypt PROPERTY IMPORTED_LOCATION ${deps_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}crypt${CMAKE_STATIC_LIBRARY_SUFFIX})
+set_property(TARGET item PROPERTY IMPORTED_LOCATION ${deps_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}item${CMAKE_STATIC_LIBRARY_SUFFIX})
+set_property(TARGET uzmtp PROPERTY IMPORTED_LOCATION ${deps_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}uzmtp${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+set_property(TARGET atx PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${deps_INSTALL_DIR}/include)
+set_property(TARGET osal PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${deps_INSTALL_DIR}/include)
+set_property(TARGET crypt PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${deps_INSTALL_DIR}/include)
+set_property(TARGET item PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${deps_INSTALL_DIR}/include)
+set_property(TARGET uzmtp PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${deps_INSTALL_DIR}/include)
+
+add_dependencies(atx atx-project)
+add_dependencies(osal atx-project)
+add_dependencies(crypt atx-project)
+add_dependencies(item atx-project)
+add_dependencies(uzmtp atx-project)
