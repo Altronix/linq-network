9d6783da63eb841345bfcefcf101ba6f3e6edb3a
thomas <thomas.chiantia@gmail.com>
2019-07-13 08:26:14 -0400

    stubs

--- cmake/klib.cmake
+++ cmake/klib.cmake
@@ -1,8 +1,10 @@
 # klib
 
 file(MAKE_DIRECTORY ${deps_INSTALL_DIR}/include/klib)
-add_custom_command(OUTPUT ${deps_INSTALL_DIR}/include/klib/khash.h
- 	COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/external/klib/khash.h ${deps_INSTALL_DIR}/include/klib)
+add_custom_command(
+	OUTPUT ${deps_INSTALL_DIR}/include/klib/khash.h ${deps_INSTALL_DIR}/include/klib/klist.h
+ 	COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/external/klib/khash.h ${deps_INSTALL_DIR}/include/klib
+ 	COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/external/klib/klist.h ${deps_INSTALL_DIR}/include/klib)
 
 add_custom_target(klib-install DEPENDS ${deps_INSTALL_DIR}/include/klib/khash.h)
 add_library(klib INTERFACE)
