d04044ae2eef6551b32c12cfe1f984e1521fe643
thomas <thomas.chiantia@gmail.com>
2019-07-17 09:43:47 -0400

    file adds

---
+++ src/node.h
@@ -0,0 +1,16 @@
+#ifndef NODE_H_
+#define NODE_H_
+
+#include "czmq.h"
+#include "linq_internal.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NODE_H_ */
---
+++ src/nodes.h
@@ -0,0 +1,16 @@
+#ifndef NODES_H_
+#define NODES_H_
+
+#include "czmq.h"
+#include "linq_internal.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NODES_H_ */
