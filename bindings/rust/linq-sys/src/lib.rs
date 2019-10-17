267d089c1bf397d84b7c25a5e2c2a268de993b17
thomas <thomas.chiantia@gmail.com>
2019-10-17 16:58:50 -0400

    compile linq-c with build.rs

--- bindings/rust/linq-sys/src/lib.rs
+++ bindings/rust/linq-sys/src/lib.rs
@@ -1,7 +1,15 @@
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+
+        unsafe {
+            let l = linq_create(std::ptr::null_mut(), std::ptr::null_mut());
+        }
     }
 }
