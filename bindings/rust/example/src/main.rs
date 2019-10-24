5231568d2bd6fc8f49e204f17b9b54f9962419e4
thomas <thomas.chiantia@gmail.com>
2019-10-24 13:53:59 -0400

    polling

--- bindings/rust/example/src/main.rs
+++ bindings/rust/example/src/main.rs
@@ -8,7 +8,12 @@ fn main() {
     linq.on_alert(|_l, sid| println!("[ALERT] {}", sid));
     linq.on_error(|_l, _e, sid| println!("[ALERT] {}", sid));
 
-    // ...
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(250));
+        if !(linq.poll() == 0) {
+            break;
+        };
+    }
 
     linq.shutdown(socket);
 }
