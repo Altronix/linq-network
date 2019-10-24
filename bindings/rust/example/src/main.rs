f856a3d54ca05bb590c31d6be97bd4e9733a397c
thomas <thomas.chiantia@gmail.com>
2019-10-24 13:45:42 -0400

    ...

--- bindings/rust/example/src/main.rs
+++ bindings/rust/example/src/main.rs
@@ -1,8 +1,12 @@
 extern crate linq;
 
 fn main() {
-    let linq = linq::Linq::new();
+    let mut linq = linq::Linq::new();
     let socket = linq.listen("tcp://*:33455");
+
+    linq.on_heartbeat(|_l, sid| println!("[HEARTBEAT] {}", sid));
+    linq.on_alert(|_l, sid| println!("[ALERT] {}", sid));
+    linq.on_error(|_l, _e, sid| println!("[ALERT] {}", sid));
 
     // ...
 
