b525d2e687f20caad74848bd318002332e5748db
thomas <thomas.chiantia@gmail.com>
2019-11-07 14:03:03 -0500

    multi threaded example

--- bindings/rust/example/src/main.rs
+++ bindings/rust/example/src/main.rs
@@ -1,21 +1,37 @@
 extern crate linq;
+extern crate rocket;
 
 static PORT: u32 = 33455;
+
+use std::sync::{Arc, Mutex};
 
 fn main() {
     // Setup Linq
     println!("Listening on port {}", PORT);
-    linq::init()
-        .register(linq::Event::on_heartbeat(move |l, sid| {
-            println!("[HEARTBEAT] {}", sid);
-            l.send(linq::Request::Get("/ATX/about"), sid, |e, json| {
-                println!("[RESPONSE] {}, {}", e, json);
-            });
-        }))
-        .register(linq::Event::on_alert(|_l, sid| println!("[ALERT] {}", sid)))
-        .register(linq::Event::on_error(|_l, e, _sid| {
-            println!("[ERROR] {}", e)
-        }))
-        .listen(linq::Endpoint::Tcp(PORT))
-        .start();
+    let l = Arc::new(Mutex::new(
+        linq::init()
+            .register(linq::Event::on_heartbeat(move |l, sid| {
+                println!("[S] Received HEARTBEAT from [{}]", sid);
+                l.send(linq::Request::Get("/ATX/about"), sid, |_e, json| {
+                    println!("[S] Received RESPONSE from [{}]\n{}", "TODO", json);
+                });
+            }))
+            .register(linq::Event::on_alert(|_l, sid| {
+                println!("[S] Received ALERT from [{}]", sid)
+            }))
+            .register(linq::Event::on_error(|_l, e, _sid| {
+                println!("[S] Received ERROR [{}]", e)
+            }))
+            .listen(linq::Endpoint::Tcp(PORT)),
+    ));
+
+    let linq = Arc::clone(&l);
+    let t = std::thread::spawn(move || {
+        while linq::running() {
+            let linq = linq.lock().unwrap();
+            linq.poll(200);
+        }
+    });
+
+    t.join().unwrap();
 }
