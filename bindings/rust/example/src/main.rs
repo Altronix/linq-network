bc181d28da2ca6392f4ebb38d2ed213889fbf1c7
thomas <thomas.chiantia@gmail.com>
2019-11-07 19:31:03 -0500

    multi-threaded example

--- bindings/rust/example/src/main.rs
+++ bindings/rust/example/src/main.rs
@@ -6,41 +6,53 @@ extern crate rocket;
 extern crate linq;
 
 // use rocket::response::content;
+use linq::{Endpoint, Event, LinqConnection, Request};
 use rocket::State;
 use std::sync::{Arc, Mutex};
 
 static PORT: u32 = 33455;
 
 #[get("/linq")]
-fn linq_route(linq: State<Mutex<linq::LinqConnection>>) -> String {
+fn linq_route(linq: State<Arc<Mutex<LinqConnection>>>) -> String {
     linq.lock().unwrap().ctx.device_count().to_string()
+}
+
+struct Counter {
+    x: u32,
+}
+
+#[get("/counter")]
+fn counter(c: State<Mutex<Counter>>) -> String {
+    let mut c = c.lock().unwrap();
+    c.x.to_string()
 }
 
 fn main() {
     // Setup Linq
     println!("Listening on port {}", PORT);
     let conn = linq::init()
-        .register(linq::Event::on_heartbeat(move |l, sid| {
+        .register(Event::on_heartbeat(move |l, sid| {
             println!("[S] Received HEARTBEAT from [{}]", sid);
-            l.send(linq::Request::Get("/ATX/about"), sid, |_e, json| {
+            l.send(Request::Get("/ATX/about"), sid, |_e, json| {
                 println!("[S] Received RESPONSE from [{}]\n{}", "TODO", json);
             });
         }))
-        .register(linq::Event::on_alert(|_l, sid| {
+        .register(Event::on_alert(|_l, sid| {
             println!("[S] Received ALERT from [{}]", sid)
         }))
-        .register(linq::Event::on_error(|_l, e, _sid| {
+        .register(Event::on_error(|_l, e, _sid| {
             println!("[S] Received ERROR [{}]", e)
         }))
-        .listen(linq::Endpoint::Tcp(PORT));
+        .listen(Endpoint::Tcp(PORT));
 
-    // let linq = Arc::new(Mutex::new(linq));
-    // let rocket_linq = Arc::clone(&linq);
-    // let t = std::thread::spawn(move || linq::task(Arc::clone(&linq)));
+    let conn = Arc::new(Mutex::new(conn));
+    let rocket_linq = Arc::clone(&conn);
+    let t = std::thread::spawn(move || linq::task(Arc::clone(&conn)));
 
     rocket::ignite()
-        .mount("/", routes![linq_route])
-        .manage(Mutex::new(conn))
+        .mount("/", routes![linq_route, counter])
+        .manage::<Arc<Mutex<LinqConnection>>>(rocket_linq)
+        .manage(Mutex::new(Counter { x: 0 }))
         .launch();
 
     // t.join().unwrap();
