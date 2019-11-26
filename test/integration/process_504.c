5c8037d593d48270818d9a67be8c7af1f02d0fa2
thomas <thomas.chiantia@gmail.com>
2019-11-26 16:05:24 -0500

    added test stub for processing 504

--- test/integration/process_504.c
+++ test/integration/process_504.c
@@ -13,13 +13,10 @@ on_request_complete(void* pass, E_LINQ_ERROR e, const char* json, device_s** d)
     ((void)e);
     ((void)json);
     ((void)d);
-    if (!e && !memcmp("{\"hello\":\"world\"}", json, 17)) {
-        *((bool*)pass) = true;
-        printf("%s", "[C] received response");
-    } else {
-        printf("%s", "[C] received response error!");
-    }
+    *((bool*)pass) = true;
 }
+
+static int PORT = 32820;
 
 int
 main(int argc, char* argv[])
@@ -30,46 +27,46 @@ main(int argc, char* argv[])
     linq_netw_socket s;
     bool pass = false;
 
-    err = 0; // TODO
-
-    fixture_context* fixture = fixture_create("dummy", 32820);
+    // Create test fixture
+    fixture_context* fixture = fixture_create("dummy", PORT);
     if (!fixture) return -1;
 
+    // Create server
     linq_netw_s* server = linq_netw_create(NULL, NULL);
     if (!server) {
         fixture_destroy(&fixture);
         return -1;
     }
 
-    s = linq_netw_listen(server, "tcp://127.0.0.1:32820");
+    // Listen to port
+    char endpoint[32];
+    snprintf(endpoint, sizeof(endpoint), "tcp://127.0.0.1:%d", PORT);
+    s = linq_netw_listen(server, endpoint);
     if (s == LINQ_ERROR_SOCKET) {
-        printf("%s", "[S] Listen Failure!\n");
         fixture_destroy(&fixture);
         linq_netw_destroy(&server);
         return -1;
     }
 
+    // Wait for device to connect to us. Then send a request to the test
+    // endpoint that sends us 504 as a response a few times.
     bool request_sent = false;
-    while (!(pass)) {
+    while (!pass) {
         fixture_poll(fixture);
-        err = linq_netw_poll(server, 5);
-        if (err) break;
-        err = linq_netw_poll(server, 5);
-        if (err) break;
-
+        if (linq_netw_poll(server, 0)) break;
         if (!request_sent && linq_netw_device_count(server)) {
-            printf("%s", "[C] Request Sent!");
             linq_netw_device_send_get(
-                server, "dummy", "/ATX/hello", on_request_complete, &pass);
+                server,
+                "dummy",
+                "/ATX/test/error_504",
+                on_request_complete,
+                &pass);
             request_sent = true;
+            printf("%s", "[C] Request Sent!");
         }
     }
 
-    err = pass ? 0 : -1;
-
     fixture_destroy(&fixture);
     linq_netw_destroy(&server);
-
-    return err;
+    return pass ? 0 : -1;
 }
-
