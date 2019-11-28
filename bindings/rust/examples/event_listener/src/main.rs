d3b0acb6d816fd56863df62a5525e5fa00b9fe33
thomas <thomas.chiantia@gmail.com>
2019-11-28 08:35:13 -0500

    async_std::main for example

--- bindings/rust/examples/event_listener/src/main.rs
+++ bindings/rust/examples/event_listener/src/main.rs
@@ -1,22 +1,22 @@
 #![feature(proc_macro_hygiene)]
+#![feature(rustc_private)]
 #![feature(decl_macro)]
 #![feature(async_closure)]
 
 extern crate linq_netw;
 use linq_netw::{Endpoint, Event};
 
-use futures::executor::block_on;
 use futures::prelude::*;
 use futures::stream::StreamExt;
 
 static PORT: u32 = 33455;
 
-fn main() {
+#[async_attributes::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let linq = linq_netw::arc::Context::new();
     linq.listen(Endpoint::Tcp(PORT));
 
-    let events = linq
-        .events()
+    linq.events()
         .take_while(|e| future::ready(*e != Event::Ctrlc))
         .for_each(async move |e| {
             match e {
@@ -25,7 +25,7 @@ fn main() {
                 Event::Error(_, _) => println!("[RECEIVED ERROR]"),
                 _ => (),
             };
-        });
-
-    block_on(events);
+        })
+        .await;
+    Ok(())
 }
