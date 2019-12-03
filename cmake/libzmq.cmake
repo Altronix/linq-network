cd30a4e6a6c545c61983afeb801196e19f7be518
thomas <thomas@altronix.com>
2019-12-03 17:18:14 -0500

    build our dependency in release mode

--- cmake/libzmq.cmake
+++ cmake/libzmq.cmake
@@ -4,6 +4,12 @@ ExternalProject_Add(zmq-project
 	SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/libzmq
 	INSTALL_DIR ${deps_INSTALL_DIR}
 	UPDATE_COMMAND ""
+	BUILD_COMMAND ""
+	INSTALL_COMMAND
+		cmake
+		--build .
+		--target install
+		--config Release
 	LIST_SEPARATOR |
 	CMAKE_ARGS 
 		-DCMAKE_INSTALL_PREFIX=${deps_INSTALL_DIR} 
@@ -21,7 +27,7 @@ set(zmq_INCLUDE_DIR ${install_dir}/include)
 IF(NOT MSVC)
   set(zmq_LIBRARY ${install_dir}/lib64/${CMAKE_STATIC_LIBRARY_PREFIX}zmq${CMAKE_STATIC_LIBRARY_SUFFIX})
 ELSE()
-  set(zmq_LIBRARY ${install_dir}/lib/libzmq-v142-mt-sgd-4_3_3${CMAKE_STATIC_LIBRARY_SUFFIX})
+  set(zmq_LIBRARY ${install_dir}/lib/libzmq-v142-mt-s-4_3_3${CMAKE_STATIC_LIBRARY_SUFFIX})
 ENDIF()
 
 add_library(zmq STATIC IMPORTED)
