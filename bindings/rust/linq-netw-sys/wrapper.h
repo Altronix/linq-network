6d4654eebc343d248b53ba489acc4f7ed1cf15ff
thomas <thomas.chiantia@gmail.com>
2019-12-07 10:44:18 -0500

    fixed nested include issue

--- bindings/rust/linq-netw-sys/wrapper.h
+++ bindings/rust/linq-netw-sys/wrapper.h
@@ -2,4 +2,9 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#ifndef WRAPPER_H_
+#define WRAPPER_H_
+
 #include "../../../include/altronix/linq_netw.h"
+
+#endif
