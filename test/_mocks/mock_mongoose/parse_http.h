45989e9ac7a538fbbaf6daa5741cf9b11aba2d9e
thomas <thomas.chiantia@gmail.com>
2019-12-14 17:54:19 -0500

    added license to new files

--- test/_mocks/mock_mongoose/parse_http.h
+++ test/_mocks/mock_mongoose/parse_http.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2019-2020 Altronix Corp
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef PARSE_HTTP_RESPONSE_H_
 #define PARSE_HTTP_RESPONSE_H_
 
