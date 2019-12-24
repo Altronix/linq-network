a92ca8f251ccd36e35bc166f052947aea34ff73d
thomas <thomas.chiantia@gmail.com>
2019-12-24 15:28:02 -0500

    refactored routes test

--- test/routes/main.c
+++ test/routes/main.c
@@ -8,7 +8,9 @@
 
 #include <cmocka.h>
 
-static void
+#include "devices.h"
+
+void
 test_init()
 {
     mongoose_spy_init();
@@ -18,109 +20,11 @@ test_init()
     sqlite_spy_step_return_push(SQLITE_ROW);  // device database OK
 }
 
-static void
+void
 test_reset()
 {
     mongoose_spy_deinit();
     sqlite_spy_deinit();
-}
-
-static void
-test_route_devices(void** context_p)
-{
-    ((void)context_p);
-    const char* body_expect =
-        "{"
-        "\"devices\":{"
-        "\"A\":{\"product\":\"B\",\"prj_version\":\"C\",\"atx_version\":\"D\"},"
-        "\"A\":{\"product\":\"B\",\"prj_version\":\"C\",\"atx_version\":\"D\"},"
-        "\"A\":{\"product\":\"B\",\"prj_version\":\"C\",\"atx_version\":\"D\"}"
-        "}}";
-
-    test_init();
-    linq_netw_s* l = linq_netw_create(NULL, NULL);
-    linq_netw_listen(l, "tcp://*:32820");
-    linq_netw_listen(l, "http://*:8000");
-    sqlite_spy_outgoing_statement_flush();
-
-    sqlite_spy_step_return_push(SQLITE_ROW);
-    sqlite_spy_step_return_push(SQLITE_ROW);
-    sqlite_spy_step_return_push(SQLITE_ROW);
-    sqlite_spy_column_text_return_push("A");
-    sqlite_spy_column_text_return_push("B");
-    sqlite_spy_column_text_return_push("C");
-    sqlite_spy_column_text_return_push("D");
-    sqlite_spy_column_text_return_push("A");
-    sqlite_spy_column_text_return_push("B");
-    sqlite_spy_column_text_return_push("C");
-    sqlite_spy_column_text_return_push("D");
-    sqlite_spy_column_text_return_push("A");
-    sqlite_spy_column_text_return_push("B");
-    sqlite_spy_column_text_return_push("C");
-    sqlite_spy_column_text_return_push("D");
-    mongoose_spy_event_request_push(
-        "", "GET", "/api/v1/linq-lite/devices", NULL);
-    for (int i = 0; i < 4; i++) linq_netw_poll(l, -1);
-
-    mongoose_parser_context* response = mongoose_spy_response_pop();
-    assert_non_null(response);
-    assert_memory_equal(response->body, body_expect, strlen(body_expect));
-    mock_mongoose_response_destroy(&response);
-
-    linq_netw_destroy(&l);
-    test_reset();
-}
-
-static void
-test_route_devices_response_too_large(void** context_p)
-{
-    ((void)context_p);
-    const char* body_expect = "{\"error\":\"Response too large\"}";
-
-    test_init();
-    linq_netw_s* l = linq_netw_create(NULL, NULL);
-    linq_netw_listen(l, "tcp://*:32820");
-    linq_netw_listen(l, "http://*:8000");
-    sqlite_spy_outgoing_statement_flush();
-
-    for (int i = 0; i < 10000; i++) sqlite_spy_step_return_push(SQLITE_ROW);
-    mongoose_spy_event_request_push(
-        "", "GET", "/api/v1/linq-lite/devices", NULL);
-    for (int i = 0; i < 4; i++) linq_netw_poll(l, -1);
-
-    mongoose_parser_context* response = mongoose_spy_response_pop();
-    assert_non_null(response);
-    assert_memory_equal(response->body, body_expect, strlen(body_expect));
-    mock_mongoose_response_destroy(&response);
-
-    linq_netw_destroy(&l);
-    test_reset();
-}
-
-static void
-test_route_devices_response_get_only(void** context_p)
-{
-    ((void)context_p);
-    const char* body_expect = "{\"error\":\"Bad request\"}";
-
-    test_init();
-    linq_netw_s* l = linq_netw_create(NULL, NULL);
-    linq_netw_listen(l, "tcp://*:32820");
-    linq_netw_listen(l, "http://*:8000");
-    sqlite_spy_outgoing_statement_flush();
-
-    sqlite_spy_column_text_return_push("A");
-    mongoose_spy_event_request_push(
-        "", "POST", "/api/v1/linq-lite/devices", "{\"blah\":\"blah\"}");
-    for (int i = 0; i < 4; i++) linq_netw_poll(l, -1);
-
-    mongoose_parser_context* response = mongoose_spy_response_pop();
-    assert_non_null(response);
-    assert_memory_equal(response->body, body_expect, strlen(body_expect));
-    mock_mongoose_response_destroy(&response);
-
-    linq_netw_destroy(&l);
-    test_reset();
 }
 
 int
---
+++ test/routes/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "sys.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void test_init();
+    void test_reset();
+
+#ifdef __cplusplus
+}
+#endif
+#endif
