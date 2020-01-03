443f6087b1b09e35ac890565ee158f50a43fe4b5
thomas <thomas.chiantia@gmail.com>
2020-01-03 16:33:23 -0500

    publish attempt

--- bindings/nodejs/scripts/read_zmq_version.ps1
+++
@@ -1,6 +0,0 @@
-$path = $args[0]
-$MAJOR = cat $path | Select-String -Pattern "^#define +ZMQ_VERSION_MAJOR +[0-9]+$" | %{ $_.Line.Split(' ')[2]; }
-$MINOR = cat $path | Select-String -Pattern "^#define +ZMQ_VERSION_MINOR +[0-9]+$" | %{ $_.Line.Split(' ')[2]; }
-$PATCH = cat $path | Select-String -Pattern "^#define +ZMQ_VERSION_PATCH +[0-9]+$" | %{ $_.Line.Split(' ')[2]; }
-$VERSION = "$($MAJOR)_$($MINOR)_$($PATCH)"
-Write-Output "$($VERSION)"
