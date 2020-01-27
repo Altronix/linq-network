04109b2bac1caeae9e69f3011465a370007ee1d0
thomas <thomas.chiantia@gmail.com>
2020-01-27 12:14:09 -0500

    bump wolfssl dependency and wolfssl cmake'

--- cmake/wolfssl.cmake
+++ cmake/wolfssl.cmake
@@ -1,34 +1,40 @@
 set(wolfssl_SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/wolfssl)
 
-### Build Wolfssl ###
-ExternalProject_Add(wolfssl-project
-	SOURCE_DIR ${wolfssl_SOURCE_DIR}
-	INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-	BUILD_IN_SOURCE 1
-	UPDATE_COMMAND ""
-	PREFIX ${CMAKE_INSTALL_PREFIX}
-	CONFIGURE_COMMAND ./autogen.sh
-		COMMAND ./configure --prefix=<INSTALL_DIR>
-		--enable-debug
-		--enable-static=yes
-		--enable-shared=yes
-		--enable-oldtls=no
-		--enable-keygen
-		--enable-certgen
-		--enable-testcert
-                --enable-opensslextra
-	BUILD_COMMAND make
-	INSTALL_COMMAND make install
-	EXCLUDE_FROM_ALL true)
-ExternalProject_Get_Property(wolfssl-project INSTALL_DIR)
-set(wolfssl_INCLUDE_DIR ${INSTALL_DIR}/include)
-FILE(MAKE_DIRECTORY ${INSTALL_DIR}/include)
 if(NOT MSVC)
+  ### Build Wolfssl ###
+  ExternalProject_Add(wolfssl-project
+  	SOURCE_DIR ${wolfssl_SOURCE_DIR}
+  	INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+  	BUILD_IN_SOURCE 1
+  	UPDATE_COMMAND ""
+  	PREFIX ${CMAKE_INSTALL_PREFIX}
+  	CONFIGURE_COMMAND ./autogen.sh
+  		COMMAND ./configure --prefix=<INSTALL_DIR>
+  		--enable-debug
+  		--enable-static=yes
+  		--enable-shared=yes
+  		--enable-oldtls=no
+  		--enable-keygen
+  		--enable-certgen
+  		--enable-testcert
+                --enable-opensslextra
+  	BUILD_COMMAND make
+  	INSTALL_COMMAND make install
+  	EXCLUDE_FROM_ALL true)
+  ExternalProject_Get_Property(wolfssl-project INSTALL_DIR)
+  set(wolfssl_INCLUDE_DIR ${INSTALL_DIR}/include)
+  FILE(MAKE_DIRECTORY ${INSTALL_DIR}/include)
   set(wolfssl_static_LIBRARY ${CMAKE_STATIC_LIBRARY_PREFIX}wolfssl${CMAKE_STATIC_LIBRARY_SUFFIX})
   set(wolfssl_shared_LIBRARY ${CMAKE_SHARED_LIBRARY_PREFIX}wolfssl${CMAKE_SHARED_LIBRARY_SUFFIX})
   set(wolfssl_static_LIBRARY ${INSTALL_DIR}/lib/${wolfssl_static_LIBRARY})
   set(wolfssl_shared_LIBRARY ${INSTALL_DIR}/lib/${wolfssl_shared_LIBRARY})
 else()
+  # TODO add msvc_project
+  message(STATUS "Building wolfssl-fips.sln")
+  include_external_msproject(wolfssl-project ${wolfssl_SOURCE_DIR}/IDE/WIN10/wolfssl-fips.vcxproj)
+  set(wolfssl_INCLUDE_DIR ${INSTALL_DIR}/include)
+  FILE(MAKE_DIRECTORY ${INSTALL_DIR}/include)
+  set(wolfssl_static_LIBRARY ${CMAKE_STATIC_LIBRARY_PREFIX}wolfssl${CMAKE_STATIC_LIBRARY_SUFFIX})
   set(wolfssl_static_LIBRARY ${CMAKE_STATIC_LIBRARY_PREFIX}wolfssl${CMAKE_STATIC_LIBRARY_SUFFIX})
   set(wolfssl_shared_LIBRARY ${CMAKE_SHARED_LIBRARY_PREFIX}wolfssl${CMAKE_SHARED_LIBRARY_SUFFIX})
   set(wolfssl_static_LIBRARY ${INSTALL_DIR}/lib/${wolfssl_static_LIBRARY})
