d8258aedf4a298ec77a1af8dc9ad4906839cdc9a
thomas <thomas.chiantia@gmail.com>
2020-01-30 18:13:09 -0500

    file adds for last commit

---
+++ src/routes/route_users.c
@@ -0,0 +1,141 @@
+#include "atx_net_internal.h"
+#include "jsmn_helpers.h"
+#include "log.h"
+#include "routes.h"
+
+#include "openssl/sha.h"
+
+#define PASSWORD_MAX_LEN 48
+#define PASSWORD_MIN_LEN 12
+#define SALT_LEN 16
+#define HASH_LEN 65
+
+#ifdef TESTING
+static void
+gen_salt(char* dst)
+{
+    memcpy(dst, "0123456789ABCDEF", 16);
+}
+#else
+static void
+gen_salt(char* dst)
+{
+    // TODO
+    memcpy(dst, "0123456789ABCDEF", 16);
+}
+#endif
+
+static void
+hash_256(const char* data, uint32_t dlen, char buffer[HASH_LEN])
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha;
+    SHA256_Init(&sha);
+    SHA256_Update(&sha, data, dlen);
+    SHA256_Final(hash, &sha);
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        sprintf(buffer + (i * 2), "%02X", hash[i]);
+    }
+    buffer[64] = 0;
+}
+
+static int
+gen_password_hash(
+    char dst[HASH_LEN],
+    char salt[SALT_LEN],
+    const char* pass,
+    uint32_t len)
+{
+    char hasher[HASH_LEN];
+    char concat[PASSWORD_MAX_LEN + SALT_LEN];
+    int l;
+#ifdef TESTING
+    log_warn("(HTTP) generating unsafe debug hash!");
+#endif
+    if (!(len < PASSWORD_MAX_LEN && len >= PASSWORD_MIN_LEN)) return -1;
+    atx_net_assert(len < PASSWORD_MAX_LEN);
+    gen_salt(salt);
+    l = snprintf(concat, sizeof(concat), "%.*s%.*s", len, pass, SALT_LEN, salt);
+    hash_256(concat, l, dst);
+    return 0;
+}
+
+void
+route_login(
+    http_route_context* ctx,
+    HTTP_METHOD meth,
+    uint32_t l,
+    const char* body)
+{}
+
+void
+route_users(
+    http_route_context* ctx,
+    HTTP_METHOD meth,
+    uint32_t l,
+    const char* body)
+{}
+
+static void
+process_create_admin(http_route_context* ctx, uint32_t l, const char* body)
+{
+    database_s* db = atx_net_database(ctx->context);
+    char k[128], v[256], h[HASH_LEN], s[SALT_LEN];
+    uint32_t klen, vlen;
+    int count, err;
+    zuuid_t* uid = zuuid_new();
+    atx_str user, pass;
+    jsmntok_t t[20];
+
+    // clang-format off
+    count = jsmn_parse_tokens(
+        t, 20,
+        body, l,
+        2,
+        "user", user,
+        "pass", pass);
+    // clang-format on
+
+    if (!(count == 2)) {
+        http_printf_json(ctx->curr_connection, 400, JERROR_400);
+    } else {
+        log_info("(DATA) creating admin account %.*s", user.len, user.p);
+        gen_password_hash(h, s, pass.p, pass.len);
+        // clang-format off
+        klen = snprintf(k, sizeof(k), "%s", "user_id,user,pass,salt,role");
+        vlen = snprintf(
+            v,
+            sizeof(v),
+            "%s,%.*s,%.*s,%.*s,%d",
+            zuuid_str(uid),
+            user.len,
+            user.p,
+            HASH_LEN,
+            h,
+            SALT_LEN,
+            s,
+            3);
+        // clang-format on
+        err = database_insert_raw_n(db, "users", k, klen, v, vlen);
+        if (!err) {
+            http_printf_json(ctx->curr_connection, 200, JERROR_200);
+        } else {
+            http_printf_json(ctx->curr_connection, 500, JERROR_500);
+        }
+    }
+}
+
+void
+route_create_admin(
+    http_route_context* ctx,
+    HTTP_METHOD meth,
+    uint32_t l,
+    const char* body)
+{
+    // TODO - check if a user already exists
+    if (false) {
+        http_printf_json(ctx->curr_connection, 503, JERROR_503);
+    } else {
+        process_create_admin(ctx, l, body);
+    }
+}
