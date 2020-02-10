a03a73f2d5b76a9bcd43218fb31d93e1b9371922
thomas <thomas.chiantia@gmail.com>
2020-02-10 16:20:16 -0500

    add build scripts for node and rust

--- scripts/ci/build-c.sh
+++ scripts/ci/build-c.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-cmake   -DCMAKE_INSTALL_PREFIX=./build/install
-	-DENABLE_TESTING=$ENABLE_TESTING
-	-DBUILD_EXAMPLES=$BUILD_EXAMPLES
+cmake \
+	-DCMAKE_INSTALL_PREFIX=./build/install \
+	-DENABLE_TESTING=$ENABLE_TESTING \
+	-DBUILD_EXAMPLES=$BUILD_EXAMPLES \
 	-DWITH_SQLITE=$WITH_SQLITE
 
 cmake --build . --target install
@@ -10,6 +11,6 @@ cmake --build . --target install
 if [ $ENABLE_TESTING ]; then
 	ctest -T memcheck &&
 		make test-coverage &&
-          	bash <(curl -s https://codecov.io/bash) -f test-coverage.info;
+          	curl -s https://codecov.io/bash -f test-coverage.info;
 fi
 
