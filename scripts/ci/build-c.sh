05a65c6909d3bd88ef4301e24e9bc3e14c8c40cd
thomas <thomas.chiantia@gmail.com>
2020-02-10 15:02:52 -0500

    refactor travis

---
+++ scripts/ci/build-c.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cmake   -DCMAKE_INSTALL_PREFIX=./build/install
+	-DENABLE_TESTING=$ENABLE_TESTING
+	-DBUILD_EXAMPLES=$BUILD_EXAMPLES
+	-DWITH_SQLITE=$WITH_SQLITE
+
+cmake --build . --target install
+
+if [ $ENABLE_TESTING ]; then
+	ctest -T memcheck &&
+		make test-coverage &&
+          	bash <(curl -s https://codecov.io/bash) -f test-coverage.info;
+fi
+
