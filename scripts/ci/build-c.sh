1239ffe249f744eaaed7a9983ece0df8dc5ed233
thomas <thomas.chiantia@gmail.com>
2020-02-10 21:17:31 -0500

    fixed travis

--- scripts/ci/build-c.sh
+++ scripts/ci/build-c.sh
@@ -7,19 +7,25 @@ if [[ -z "${USE_SYSTEM_JSMN_WEB_TOKENS}" ]]; then USE_SYSTEM_JSMN_WEB_TOKENS=OFF
 if [[ -z "${USE_SYSTEM_OPENSSL}" ]]; then USE_SYSTEM_OPENSSL=OFF; fi
 if [[ -z "${BUILD_EXAMPLES}" ]]; then BUILD_EXAMPLES=OFF; fi
 
-cmake \
-	-DCMAKE_INSTALL_PREFIX=./build/install \
-	-DENABLE_TESTING=$ENABLE_TESTING \
-	-DBUILD_EXAMPLES=$BUILD_EXAMPLES \
-	-DWITH_SQLITE=$WITH_SQLITE \
-	-USE_SYSTEM_ZMQ=$USE_SYSTEM_ZMQ \
-	-USE_SYSTEM_JSMN_WEB_TOKENS=$USE_SYSTEM_JSMN_WEB_TOKENS \
-	-USE_SYSTEM_OPENSSL=$USE_SYSTEM_OPENSSL
+echo "ENABLE_TESTING            : ${ENABLE_TESTING}"
+echo "WITH_SQLITE               : ${WITH_SQLITE}"
+echo "USE_SYSTEM_ZMQ            : ${USE_SYSTEM_ZMQ}"
+echo "USE_SYSTEM_JSMN_WEB_TOKENS: ${USE_SYSTEM_JSMN_WEB_TOKENS}"
+echo "BUILD_EXAMPLES            : ${BUILD_EXAMPLES}"
 
-cmake --build . --target install
-
-if [ "$ENABLE_TESTING" = "ON" ]; then
-	ctest -T memcheck &&
-		make test-coverage &&
-		bash <(curl -s https://codecov.io/bash) -f test-coverage.info;
-fi
+ cmake \
+ 	-DCMAKE_INSTALL_PREFIX=./build/install \
+ 	-DENABLE_TESTING=$ENABLE_TESTING \
+ 	-DBUILD_EXAMPLES=$BUILD_EXAMPLES \
+ 	-DWITH_SQLITE=$WITH_SQLITE \
+ 	-DUSE_SYSTEM_ZMQ=$USE_SYSTEM_ZMQ \
+ 	-DUSE_SYSTEM_JSMN_WEB_TOKENS=$USE_SYSTEM_JSMN_WEB_TOKENS \
+ 	-DUSE_SYSTEM_OPENSSL=$USE_SYSTEM_OPENSSL
+ 
+ cmake --build . --target install
+ 
+ if [ "$ENABLE_TESTING" = "ON" ]; then
+ 	ctest -T memcheck &&
+ 		make test-coverage &&
+ 		bash <(curl -s https://codecov.io/bash) -f test-coverage.info;
+ fi
