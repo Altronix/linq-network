86b2b1920d4869cae5f79cfcb11e61503cefb6b3
thomas <thomas.chiantia@gmail.com>
2020-03-10 15:58:05 -0400

    fixed tls

--- bindings/nodejs/examples/daemon/index.js
+++ bindings/nodejs/examples/daemon/index.js
@@ -1,12 +1,13 @@
-let daemon = require("@altronix/linq-network-js").daemon,
+let fs = require("fs"),
+  daemon = require("@altronix/linq-network-js").daemon,
   config = {
     http: process.env.HTTP || 8000,
     https: process.env.HTTPS || 8001,
     zmtp: process.env.ZMTP || 33455,
     zmtps: process.env.ZMTPS || 33456,
-    cert: process.env.TLS_CERT || "",
-    key: process.env.TLS_KEY || "",
-    db: process.env.DATABASE || ""
+    cert: fs.readFileSync(process.env.TLS_CERT || "./unsafe-cert.pem"),
+    key: fs.readFileSync(process.env.TLS_KEY || "./unsafe-key.pem"),
+    db: process.env.DATABASE || "./db.sqlite"
   };
 
 if (!config.key.length) console.log("TLS_KEY NOT FOUND!");
