41f09daab67e1f96cb1c5d7ae7fb6a1b4ac8685e
thomas <thomas.chiantia@gmail.com>
2020-07-21 11:45:16 -0400

    export product field to nodejs binding

--- bindings/nodejs/src/linq_network_js.cc
+++ bindings/nodejs/src/linq_network_js.cc
@@ -79,6 +79,7 @@ LinqNetwork::LinqNetwork(const Napi::CallbackInfo& info)
             std::string where{ alert->where.p, alert->where.len };
             std::string mesg{ alert->mesg.p, alert->mesg.len };
             std::string name{ alert->name.p, alert->name.len };
+            std::string product{ alert->product.p, alert->product.len };
             std::string from{ email->from.p, email->from.len };
             std::string subject{ email->subject.p, email->subject.len };
             std::string user{ email->user.p, email->user.len };
@@ -99,6 +100,7 @@ LinqNetwork::LinqNetwork(const Napi::CallbackInfo& info)
             obj.Set("when", when);
             obj.Set("mesg", mesg);
             obj.Set("name", name);
+            obj.Set("product", product);
             obj.Set("serial", sid);
             obj.Set("from", from);
             obj.Set("subject", subject);
