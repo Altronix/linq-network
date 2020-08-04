f7cea2cc25c427d4d7f49009ca042173aaa51cfa
thomas <thomas.chiantia@gmail.com>
2020-08-04 11:17:37 -0400

    test http response wire protocol

--- linq_usb/common/test/main.c
+++ linq_usb/common/test/main.c
@@ -51,6 +51,44 @@ test_wire_print_http_request(void** context_p)
 }
 
 static void
+test_wire_print_http_response_alloc(void** context_p)
+{
+    uint8_t* b = NULL;
+    uint32_t l;
+    int err;
+    rlp* result;
+    err = wire_print_http_response_alloc(&b, &l, 200, "{\"error\":\"Ok\"}");
+    assert_int_equal(err, 0);
+    result = rlp_parse(b, l);
+    assert_non_null(result);
+    assert_int_equal(rlp_as_u8(rlp_at(result, 0)), 0);
+    assert_int_equal(rlp_as_u8(rlp_at(result, 1)), 0);
+    assert_int_equal(rlp_as_u16(rlp_at(result, 2)), 200);
+    assert_string_equal(rlp_as_str(rlp_at(result, 3)), "{\"error\":\"Ok\"}");
+    rlp_free(&result);
+    free(b);
+}
+
+static void
+test_wire_print_http_response(void** context_p)
+{
+    uint8_t b[256];
+    uint32_t l = sizeof(b);
+    int e;
+
+    rlp* result;
+    e = wire_print_http_response(b, &l, 200, "{\"error\":\"Ok\"}");
+    assert_int_equal(e, 0);
+    result = rlp_parse(b, l);
+    assert_non_null(result);
+    assert_int_equal(rlp_as_u8(rlp_at(result, 0)), 0);
+    assert_int_equal(rlp_as_u8(rlp_at(result, 1)), 0);
+    assert_int_equal(rlp_as_u16(rlp_at(result, 2)), 200);
+    assert_string_equal(rlp_as_str(rlp_at(result, 3)), "{\"error\":\"Ok\"}");
+    rlp_free(&result);
+}
+
+static void
 test_wire_parse(void** context_p)
 {
     uint8_t b[256];
@@ -78,6 +116,8 @@ main(int argc, char* argv[])
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_wire_print_http_request_alloc), //
         cmocka_unit_test(test_wire_print_http_request),
+        cmocka_unit_test(test_wire_print_http_response_alloc), //
+        cmocka_unit_test(test_wire_print_http_response),
         cmocka_unit_test(test_wire_parse)
     };
 
