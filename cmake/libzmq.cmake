3c72c3d948047b67ae2773753f68eab972a00a9a
thomas <thomas@altronix.com>
2021-10-27 12:33:48 -0400

    removed some unused scripts

--- cmake/libzmq.cmake
+++ cmake/libzmq.cmake
@@ -3,6 +3,7 @@
 set(ZMQ_VERSION "4.3.4")
 set(ZMQ_SOURCE_DIR "${EXTERNAL_DIR}/zeromq-${ZMQ_VERSION}")
 set(ZMQ_TEST_FILE "${ZMQ_SOURCE_DIR}/CMakeLists.txt")
+set(zmq_VERSION "4_3_4")
 check_extract("${DOWNLOAD_DIR}/zeromq-${ZMQ_VERSION}.tar.gz" "${ZMQ_TEST_FILE}")
 
 if(MSVC)
@@ -53,21 +54,11 @@ ExternalProject_Get_Property(zmq-project install_dir)
 set(zmq_INCLUDE_DIR ${install_dir}/include)
 FILE(MAKE_DIRECTORY ${install_dir}/include)
 IF(NOT MSVC)
-  # Get the version of the ZMQ library
-  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/scripts/read_zmq_version.sh
-    ${EXTERNAL_DIR}/libzmq/include/zmq.h
-    OUTPUT_VARIABLE zmq_VERSION)
   set(zmq_static_LIBRARY ${CMAKE_STATIC_LIBRARY_PREFIX}zmq${CMAKE_STATIC_LIBRARY_SUFFIX})
   set(zmq_shared_LIBRARY ${CMAKE_SHARED_LIBRARY_PREFIX}zmq${CMAKE_SHARED_LIBRARY_SUFFIX})
   set(zmq_static_LIBRARY_LOC ${install_dir}/lib/${zmq_static_LIBRARY})
   set(zmq_shared_LIBRARY_LOC ${install_dir}/lib/${zmq_shared_LIBRARY})
 ELSE()
-  execute_process(COMMAND powershell -ExecutionPolicy Bypass
-    -File "${CMAKE_SOURCE_DIR}/scripts/read_zmq_version.ps1"
-    "${ZMQ_SOURCE_DIR}/include/zmq.h"
-    OUTPUT_VARIABLE zmq_VERSION)
-  STRING(REGEX REPLACE "\n" "" zmq_VERSION ${zmq_VERSION})
-  STRING(REGEX REPLACE "\r" "" zmq_VERSION ${zmq_VERSION})
   MESSAGE(STATUS "zmq_VERSION: ${zmq_VERSION}")
   set(zmq_static_LIBRARY libzmq-${CMAKE_VS_PLATFORM_TOOLSET}-mt-s-${zmq_VERSION}${CMAKE_STATIC_LIBRARY_SUFFIX})
   set(zmq_shared_LIBRARY libzmq-${CMAKE_VS_PLATFORM_TOOLSET}-mt-${zmq_VERSION}${CMAKE_SHARED_LIBRARY_SUFFIX})
